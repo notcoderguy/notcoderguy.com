004104c06bfab93a9aac27f9a1c4c52752a81574
Vasu Grover <notcoderguy@gmail.com>
2024-10-09 01:05:08 +0530

    cleaned branch for nextjs init

--- src/utils/spotify.js
+++
@@ -1,7 +0,0 @@
-async function getNowPlaying() {
-    const response = await fetch('https://api.spotifyx.notcoderguy.com/');
-    const data = await response.json();
-    return data;
-}
-
-export { getNowPlaying };
